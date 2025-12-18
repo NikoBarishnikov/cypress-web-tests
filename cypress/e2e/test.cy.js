465e4edbf1490d5951785ddca46b2650e31817cc
Nikolay.Baryshnikov <nkolay.baryshnikov@uklon.com.ua>
2025-12-18 17:26:15 +0100

    api_test

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,3 +1,5 @@
+/// <reference types="Cypress" />
+
 import { mobileReplenishment } from "../support/pages/mobileReplenishment"
 import { basePage } from "../support/pages/basePage"
 
@@ -19,5 +21,47 @@ it('Replenishment of Ukraine mobile phone number', () => {
     mobileReplenishment.checkCurrensy('UAH')
 
   })
+
+
+  it('Example sending GET request', () => {
+    cy.request('https://next.privat24.ua/')
+      .then((response) => {
+        expect(response.status).to.eq(200);
+        cy.log(JSON.stringify(response.body));
+      });
+  });
+
+  it.only("Example sending the POST request", () => {
+
+    const requestBody = {
+      action: "info",
+      phone: "+380686979712",
+      amount: 50,
+      currency: "UAH",
+      cardCvv: "111",
+      card: "4552331448138217",
+      cardExp: "0526",
+      xref: "30da59f3836a783ca4115e0c66c50e47",
+      _: 1609157575732
+    };
+  
+    const headersData = {
+      'Content-Type': 'application/json',
+      'cookie': '_ga=GA1.2.957777321.1609142789; _gid=GA1.2.1882919372.1609142789; pubkey=a5cfd1b1e8e8082568965a627a5f9170; _gat_gtag_UA_29683426_11=1; fp=11; lfp=12/28/2020, 10:06:39 AM; pa=1609157568076.50150.1882585496550282next.privat24.ua0.49014387143519356+1'
+    };
+  
+    cy.request({
+      method: "POST",
+      url: "https://next.privat24.ua/api/p24/pub/mobipay",
+      headers: headersData,
+      body: requestBody,
+      failOnStatusCode: false // важно для отладки
+    }).then((response) => {
+      cy.log(`Status: ${response.status}`);
+      cy.log(JSON.stringify(response.body));
+    });
+  
+  });
+  
   
   
